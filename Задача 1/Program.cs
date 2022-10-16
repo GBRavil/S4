748b6967c80b89291a2eb10bed2ca9068d6769b5
Ravil <lllx2@yandex.ru>
2022-10-16 14:25:29 +0300

    2

--- Задача 1/Program.cs
+++ Задача 1/Program.cs
@@ -1,6 +1,7 @@
-﻿int [] num = new int[5];
+﻿//Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int [] num = new int[8];
 Random rnd = new Random();
 for(int i = 0; i < num.Length; i++) {
     num[i] = rnd.Next(0, 2);
     Console.WriteLine(num[i]);
-}
+}    
---
+++ Задача 2/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задать массив из 12 элементов, заполненных числами из [0,9]. 
+//Найти сумму положительных/отрицательных элементов массива
+int [] num = new int[12];
+int summin = 0;
+int summax = 0;
+Random rnd = new Random();
+for(int i = 0; i < num.Length; i++) {
+    num[i] = rnd.Next(0, 9);
+    Console.WriteLine(num[i]);
+    if(num[i]>=0) {
+        summax = summax + num[i];
+    } else {
+        summin = summin + num[i];
+    }
+
+}
+Console.WriteLine(summax);
+Console.WriteLine(summin);
