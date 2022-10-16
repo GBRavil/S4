ea52eff717bce91f5e212b9c7696e80410244588
Ravil <lllx2@yandex.ru>
2022-10-16 14:35:21 +0300

    3

---
+++ Задача 3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задать массив, заполнить случайными положительными трёхзначными числами. 
+//Показать количество нечетных\четных чисел
+int [] num = new int[5];
+Random rnd = new Random();
+int sumeven = 0;
+int sumodd = 0;
+for(int i = 0; i < num.Length; i++) {
+    num[i] = rnd.Next(100, 1000);
+    Console.WriteLine(num[i]);
+    if(num[i]%2==0) {
+        sumeven = sumeven + num[i];
+    } else {
+        sumodd = sumodd + num[i];
+    }
+}
+Console.WriteLine(sumeven);
+Console.WriteLine(sumodd);   
